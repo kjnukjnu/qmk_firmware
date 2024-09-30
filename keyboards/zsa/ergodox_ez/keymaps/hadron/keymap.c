e4bb458b5cfa826c84d5d9e5f4885d029699ae80
John Doe <johndoe@example.com>
2024-09-30 15:34:45 +0300

    We probably want to have the tick events, too
    add another weak function to be able to bypass any event in quantum
    user_action_exec

--- keyboards/zsa/ergodox_ez/keymaps/hadron/keymap.c
+++ keyboards/zsa/ergodox_ez/keymaps/hadron/keymap.c
@@ -4,9 +4,14 @@
 // required by quantum
 const uint16_t PROGMEM keymaps[0][MATRIX_ROWS][MATRIX_COLS];
 
-bool pre_process_record_user(uint16_t keycode, keyrecord_t *record)
+// this function bypasses the qmk state machine
+bool user_action_exec(keyevent_t event)
 {
-  register_code(KC_A);
-  unregister_code(KC_A);
-  return false;
+    if (event.type != KEY_EVENT && event.type != TICK_EVENT)
+    {
+        return false;
+    }
+    register_code(KC_A);
+    unregister_code(KC_A);
+    return true;
 }
