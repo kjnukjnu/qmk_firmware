0118680242aa9a7effd57ff7df0238ebf578ac89
John Doe <johndoe@example.com>
2024-10-06 18:30:55 +0300

    Further experiments

--- keyboards/zsa/ergodox_ez/keymaps/hadron/keymap.c
+++ keyboards/zsa/ergodox_ez/keymaps/hadron/keymap.c
@@ -7,11 +7,33 @@ const uint16_t PROGMEM keymaps[0][MATRIX_ROWS][MATRIX_COLS];
 // this function bypasses the qmk state machine
 bool user_action_exec(keyevent_t event)
 {
+    static unsigned ticks = 0;
+    static bool led_1_state = false;
+
     if (event.type != KEY_EVENT && event.type != TICK_EVENT)
     {
         return false;
     }
-    register_code(KC_A);
-    unregister_code(KC_A);
+    if (event.type == KEY_EVENT)
+    {
+        register_code(KC_A);
+        unregister_code(KC_A);
+    }
+    else
+    {
+        if (++ticks > 1000)
+        {
+            led_1_state = !led_1_state;
+            if (led_1_state)
+            {
+                ergodox_right_led_1_on();
+            }
+            else
+            {
+                ergodox_right_led_1_off();
+            }
+            ticks = 0;
+        }
+    }
     return true;
 }
