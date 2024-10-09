4432fa4f971f5fb55b30e1c8e962d6832fcb7880
John Doe <johndoe@example.com>
2024-10-09 20:14:52 +0300

    Use Finnish keycodes where applicable

--- keyboards/zsa/ergodox_ez/keymaps/hadron/keymap.c
+++ keyboards/zsa/ergodox_ez/keymaps/hadron/keymap.c
@@ -1,5 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "version.h"
+#include "keymap_finnish.h"
 
 // required by quantum
 const uint16_t PROGMEM keymaps[0][MATRIX_ROWS][MATRIX_COLS];
@@ -24,15 +25,15 @@ static const struct hadron_key PROGMEM keymap[][6][14] = {
     {
         {{KC_NO}, {KC_DEL}, {KC_LGUI}, {KC_LALT}, {KC_PRINT_SCREEN}, {KC_NO}, {KC_NO}, {KC_RALT}, {KC_RGUI}, {KC_NO}, {KC_SPACE}, {KC_NO}, {KC_NO}, {KC_NO}, },
 
-        {{KC_GRV}, {KC_NO /* PIPE: RALT + KC_GRAVE */}, {KC_GRAVE /* < */}, {KC_NO /* SHIFT + KC_GRAVE */}, {KC_ENTER}, {KC_NO}, {KC_NO}, {KC_NO}, {KC_NO}, {KC_NO /* layer */}, {KC_NO /* [: RALT + KC_8 */}, {KC_NO /* ]: RALT + KC_9 */}, {KC_NO /* @: RALT + KC_2 */}, {KC_NO /* dead tilde: RALT + KC_RIGHT_BRACKET */}},
+        {{FI_LABK /* § (sic!) */}, {KC_NO /* PIPE: RALT + KC_GRAVE */}, {FI_SECT /* < (sic!) */}, {KC_NO /* SHIFT + KC_GRAVE */}, {KC_ENTER}, {KC_NO}, {KC_NO}, {KC_NO}, {KC_NO}, {KC_NO /* layer */}, {KC_NO /* [: RALT + KC_8 */}, {KC_NO /* ]: RALT + KC_9 */}, {KC_NO /* @: RALT + KC_2 */}, {KC_NO /* dead tilde: RALT + KC_RIGHT_BRACKET */}},
 
-        {{KC_LSFT}, {KC_Z}, {KC_X}, {KC_C}, {KC_V}, {KC_B}, {KC_NO /* backslash: RALT + KC_MINUS */}, {KC_NO /* tilde: RALT + KC_RIGHT_BRACKET + KC_SPACE - KC_SPACE */}, {KC_N}, {KC_M}, {KC_COMMA}, {KC_DOT}, {KC_SLASH}, {KC_RSFT}, },
+        {{KC_LSFT}, {KC_Z}, {KC_X}, {KC_C}, {KC_V}, {KC_B}, {KC_NO /* backslash: RALT + KC_MINUS */}, {KC_NO /* tilde: RALT + KC_RIGHT_BRACKET + KC_SPACE - KC_SPACE */}, {KC_N}, {KC_M}, {FI_COMM}, {FI_DOT}, {FI_MINS}, {KC_RSFT}, },
 
-        {{KC_LEFT_CTRL}, {KC_A}, {KC_S}, {KC_D}, {KC_F}, {KC_G}, {KC_NO}, {KC_NO}, {KC_H}, {KC_J}, {KC_K}, {KC_L}, {KC_SEMICOLON}, {KC_NO /* ctrl/ä (KC_QUOTE) */},},
+        {{KC_LEFT_CTRL}, {KC_A}, {KC_S}, {KC_D}, {KC_F}, {KC_G}, {KC_NO}, {KC_NO}, {KC_H}, {KC_J}, {KC_K}, {KC_L}, {FI_ODIA}, {KC_NO /* ctrl/ä (KC_QUOTE) */},},
 
         {{KC_TAB}, {KC_Q}, {KC_W}, {KC_E}, {KC_R}, {KC_T}, {KC_NO /* {: RALT + KC_7 */}, {KC_NO /* }: RALT + KC_0 */}, {KC_Y}, {KC_U}, {KC_I}, {KC_O}, {KC_P}, {KC_BACKSLASH},},
 
-        {{KC_ESCAPE}, {KC_1}, {KC_2}, {KC_3}, {KC_4}, {KC_5}, {KC_RIGHT_BRACKET}, {KC_NO /* dead acute: RALT + KC_RIGHT_BRACKET */}, {KC_6}, {KC_7}, {KC_8}, {KC_9}, {KC_0}, {KC_MINUS}},
+        {{KC_ESCAPE}, {KC_1}, {KC_2}, {KC_3}, {KC_4}, {KC_5}, {FI_DIAE}, {FI_ACUT}, {KC_6}, {KC_7}, {KC_8}, {KC_9}, {KC_0}, {FI_PLUS}},
     },
 
     /* NAVIGATION */
