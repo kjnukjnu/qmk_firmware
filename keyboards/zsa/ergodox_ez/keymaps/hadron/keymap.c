43c9161a5f6a749df8ece1f20c13a69fa1ef2abf
John Doe <johndoe@example.com>
2024-11-15 18:41:49 +0200

    Use bit masks instead of bit numbers in modifiers
    add more special keys

--- keyboards/zsa/ergodox_ez/keymaps/hadron/keymap.c
+++ keyboards/zsa/ergodox_ez/keymaps/hadron/keymap.c
@@ -23,64 +23,55 @@ typedef void (*key_func_t)(key_t key);
 
 static intptr_t key_release_info[KEY_COUNT];
 
-// todo: more efficient status of modifiers
 static bool keycode_active_status[255];
-
-enum modifier_bit
-{
-    MODBIT_NONE = 0,
-    MODBIT_LSFT = 1,
-    MODBIT_RSFT,
-    MODBIT_LCTL,
-    MODBIT_RCTL,
-    MODBIT_LALT,
-    MODBIT_RALT,
-    MODBIT_LGUI,
-    MODBIT_RGUI
-};
-
-static uint8_t modifiers;
+typedef uint8_t mod_bits_t;
+static mod_bits_t modifiers;
 
 static const keycode_t PROGMEM modbit_keycodes[] =
-{ KC_NO, KC_LSFT, KC_RSFT, KC_LCTL, KC_RCTL, KC_LALT, KC_RALT, KC_LGUI, KC_RGUI };
+{ KC_LCTL, KC_LSFT, KC_LALT, KC_LGUI, KC_RCTL, KC_RSFT, KC_RALT, KC_RGUI };
 
 #define MAX_TMP_KEYCODES 10
 static dir_keycode_t tmp_keycodes[MAX_TMP_KEYCODES];
 static int tmp_keycode_count;
 
-static enum modifier_bit keycode_modbit(keycode_t code)
+static mod_bits_t keycode_modbit(keycode_t code)
 {
     switch (code)
     {
-    case KC_LSFT:
-        return MODBIT_LSFT;
-    case KC_RSFT:
-        return MODBIT_RSFT;
     case KC_LCTL:
-        return MODBIT_LCTL;
-    case KC_RCTL:
-        return MODBIT_RCTL;
+        return MOD_BIT_LCTRL;
+    case KC_LSFT:
+        return MOD_BIT_LSHIFT;
     case KC_LALT:
-        return MODBIT_LALT;
-    case KC_RALT:
-        return MODBIT_RALT;
+        return MOD_BIT_LALT;
     case KC_LGUI:
-        return MODBIT_LGUI;
+        return MOD_BIT_LGUI;
+    case KC_RCTL:
+        return MOD_BIT_RCTRL;
+    case KC_RSFT:
+        return MOD_BIT_RSHIFT;
+    case KC_RALT:
+        return MOD_BIT_RALT;
     case KC_RGUI:
-        return MODBIT_RGUI;
+        return MOD_BIT_RGUI;
     default:
-        return MODBIT_NONE;
+        return 0;
     };
 }
 
-static keycode_t modbit_keycode(enum modifier_bit modbit)
+static keycode_t modbit_keycode(int bit_number)
 {
-    return modbit_keycodes[modbit];
+    return modbit_keycodes[bit_number];
 }
 
 static bool keycode_active(keycode_t code)
 {
     return keycode_active_status[code];
+}
+
+static dir_keycode_t release_keycode(keycode_t code)
+{
+    return -(dir_keycode_t)code;
 }
 
 static keycode_t keycode_plain(dir_keycode_t dir_code)
@@ -96,9 +87,9 @@ static bool keycode_is_press(dir_keycode_t dir_code)
 static void keycode_send(dir_keycode_t dir_code)
 {
     keycode_t code = keycode_plain(dir_code);
-    enum modifier_bit modbit = keycode_modbit(code);
+    mod_bits_t modbit = keycode_modbit(code);
     if (dir_code == KC_NO ||
-        keycode_active_status[code] == keycode_is_press(dir_code))
+        keycode_active(code) == keycode_is_press(dir_code))
     {
         return;
     }
@@ -111,7 +102,7 @@ static void keycode_send(dir_keycode_t dir_code)
         unregister_code(code);
     }
     keycode_active_status[code] = keycode_is_press(dir_code);
-    if (modbit != MODBIT_NONE)
+    if (modbit != 0)
     {
         if (keycode_is_press(dir_code))
         {
@@ -143,41 +134,47 @@ static void tmp_keycode(dir_keycode_t dir_code)
     tmp_keycodes[tmp_keycode_count++] = dir_code;
 }
 
-static bool shift_active(void)
+static void tmp_modifiers_and_keycode(mod_bits_t mod_bits, keycode_t code)
 {
-    return keycode_active(KC_LSFT) || keycode_active(KC_RSFT);
+    mod_bits_t remove = modifiers & ~mod_bits;
+    mod_bits_t add = mod_bits & ~modifiers;
+    tmp_keycode(release_keycode(code));
+    for (int ix = 0; remove; ++ix, remove >>= 1)
+    {
+        if (remove & 1)
+        {
+            tmp_keycode(release_keycode(modbit_keycode(ix)));
+        }
+    }
+    for (int ix = 0; add; ++ix, add >>= 1)
+    {
+        if (add & 1)
+        {
+            tmp_keycode(modbit_keycode(ix));
+        }
+    }
+    tmp_keycode(code);
 }
 
-static bool ctrl_alt_gui_active(void)
-{
-    return keycode_active(KC_LCTL) || keycode_active(KC_RCTL) ||
-        keycode_active(KC_LALT) || keycode_active(KC_RALT) ||
-        keycode_active(KC_LGUI) || keycode_active(KC_RGUI);
-}
-
-static void navigation_layer_off(key_t key)
+static void k_navigation_layer_off(key_t key)
 {
     ergodox_right_led_3_off();
     layer = BASE;
 }
 
-static void navigation_layer_on(key_t key)
+static void k_navigation_layer_on(key_t key)
 {
     ergodox_right_led_3_on();
     layer = NAVIGATION;
-    key_release_info[key] = KCFUNC(navigation_layer_off);
+    key_release_info[key] = KCFUNC(k_navigation_layer_off);
 }
 
-static void four_dollar(key_t key)
+static void k_four_dollar(key_t key)
 {
-    if (shift_active() & !ctrl_alt_gui_active())
+    if ((modifiers & (MOD_BIT_LSHIFT | MOD_BIT_RSHIFT)) &&
+        !(modifiers & (MOD_BIT_LCTRL | MOD_BIT_LALT | MOD_BIT_LGUI | MOD_BIT_RCTRL | MOD_BIT_RALT | MOD_BIT_RGUI)))
     {
-        /* TODO: instead of sending tmp keycodes, give the desired modifier
-         * setup and keycode(s) after that. */
-        tmp_keycode(-KC_LSFT);
-        tmp_keycode(-KC_RSFT);
-        tmp_keycode(KC_RALT);
-        tmp_keycode(KC_4);
+        tmp_modifiers_and_keycode(MOD_BIT_RALT, KC_4);
     }
     else
     {
@@ -185,18 +182,30 @@ static void four_dollar(key_t key)
     }
 }
 
+static void k_brace_left(key_t key) {tmp_modifiers_and_keycode(MOD_BIT_RALT, KC_7);}
+static void k_brace_right(key_t key) {tmp_modifiers_and_keycode(MOD_BIT_RALT, KC_0);}
+static void k_backslash(key_t key) {tmp_modifiers_and_keycode(MOD_BIT_RALT, FI_PLUS);}
+static void k_pipe(key_t key) {tmp_modifiers_and_keycode(MOD_BIT_RALT, FI_LABK);}
+static void k_greater_than(key_t key) {tmp_modifiers_and_keycode(MOD_BIT_LSHIFT, FI_LABK);}
+static void k_lbracket(key_t key) {tmp_modifiers_and_keycode(MOD_BIT_RALT, KC_8);}
+static void k_rbracket(key_t key) {tmp_modifiers_and_keycode(MOD_BIT_RALT, KC_9);}
+static void k_ad(key_t key) {tmp_modifiers_and_keycode(MOD_BIT_RALT, KC_2);}
+static void k_dead_tilde(key_t key) {tmp_modifiers_and_keycode(MOD_BIT_RALT, FI_DIAE);}
+static void k_prev_word(key_t key) {tmp_modifiers_and_keycode(MOD_BIT_LCTRL, KC_LEFT);}
+static void k_next_word(key_t key) {tmp_modifiers_and_keycode(MOD_BIT_LCTRL, KC_RIGHT);}
+
 static const intptr_t PROGMEM keymap[][KEY_COUNT] = {
     /* BASE */
     {
-        KC_ESC, KC_1, KC_2, KC_3, KCFUNC(four_dollar), KC_5, FI_DIAE, FI_ACUT, KC_6, KC_7, KC_8, KC_9, KC_0, FI_PLUS,
+        KC_ESC, KC_1, KC_2, KC_3, KCFUNC(k_four_dollar), KC_5, FI_DIAE, FI_ACUT, KC_6, KC_7, KC_8, KC_9, KC_0, FI_PLUS,
 
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_NO /* : RALT + KC_7 */, KC_NO /* : RALT + KC_0 */, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KCFUNC(k_brace_left), KCFUNC(k_brace_right), KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
 
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_NO, KC_NO, KC_H, KC_J, KC_K, KC_L, FI_ODIA, KC_RCTL /* ctrl/ä (FI_ADIA)) */,
 
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_NO /* backslash: RALT + KC_MINUS */, KC_NO /* tilde: RALT + KC_RIGHT_BRACKET + KC_SPACE - KC_SPACE */, KC_N, KC_M, FI_COMM, FI_DOT, FI_MINS, KC_RSFT,
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KCFUNC(k_backslash), KC_NO /* tilde: RALT + FI_DIAE + KC_SPACE - KC_SPACE */, KC_N, KC_M, FI_COMM, FI_DOT, FI_MINS, KC_RSFT,
 
-        FI_SECT, KC_NO /* PIPE: RALT + FI_LABK */, FI_LABK, KC_NO /* >: SHIFT + FI_LABK */, KC_ENT, KC_NO, KC_NO, KC_NO, KC_NO, KCFUNC(navigation_layer_on), KC_NO /* [: RALT + KC_8 */, KC_NO /* ]: RALT + KC_9 */, KC_NO /* @: RALT + KC_2 */, KC_NO /* dead tilde: RALT + KC_RIGHT_BRACKET */,
+        FI_SECT, KCFUNC(k_pipe), FI_LABK, KCFUNC(k_greater_than), KC_ENT, KC_NO, KC_NO, KC_NO, KC_NO, KCFUNC(k_navigation_layer_on), KCFUNC(k_lbracket), KCFUNC(k_rbracket), KCFUNC(k_ad), KCFUNC(k_dead_tilde),
 
         KC_NO, KC_DEL, KC_LGUI, KC_LALT, FI_ARNG, KC_NO, KC_MUTE, KC_RALT, KC_RGUI, KC_NO, KC_SPC, KC_NO, KC_BSPC, KC_NO,
     },
@@ -205,7 +214,7 @@ static const intptr_t PROGMEM keymap[][KEY_COUNT] = {
     {
         KC_ESC, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_NO, KC_NO, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
 
-        KC_TAB, KC_PGUP, KC_NO /* CTRL + LEFT: prev word/CTRL-W*/, KC_UP, KC_NO /* CTRL + RIGHT next word*/, KC_NO, KC_NO /* : RALT + KC_7 */, KC_NO /* : RALT + KC_0 */, KC_Y, KC_BTN2, KC_MS_U, KC_BTN1, KC_NO, KC_F12,
+        KC_TAB, KC_PGUP, KCFUNC(k_prev_word), KC_UP, KCFUNC(k_next_word), KC_NO, KCFUNC(k_brace_left), KCFUNC(k_brace_right), KC_Y, KC_BTN2, KC_MS_U, KC_BTN1, KC_NO, KC_F12,
 
         KC_LCTL, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END, KC_NO, KC_NO, KC_WBAK, KC_MS_L, KC_MS_D, KC_MS_R, KC_NO /*ctrl alt*/, KC_RCTL /*ctrl/Ä (FI_ADIA)*/,
 
