acdc7b5f5f8bcc5ead631c653b2e67234cf6a0d8
John Doe <johndoe@example.com>
2024-11-16 18:21:35 +0200

    Fix the shift-4 to produce $ again
    now that the existing modifiers are not cleaned automatically

--- keyboards/zsa/ergodox_ez/keymaps/hadron/keymap.c
+++ keyboards/zsa/ergodox_ez/keymaps/hadron/keymap.c
@@ -168,6 +168,8 @@ static void k_four_dollar(key_t key)
     if ((modifiers & (MOD_BIT_LSHIFT | MOD_BIT_RSHIFT)) &&
         !(modifiers & (MOD_BIT_LCTRL | MOD_BIT_LALT | MOD_BIT_LGUI | MOD_BIT_RCTRL | MOD_BIT_RALT | MOD_BIT_RGUI)))
     {
+        tmp_keycode(release_keycode(KC_LSFT));
+        tmp_keycode(release_keycode(KC_RSFT));
         tmp_modifiers_and_keycode(MOD_BIT_RALT, KC_4);
     }
     else
